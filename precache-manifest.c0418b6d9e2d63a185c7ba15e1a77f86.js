050a0b73f68703c091dbd0f2335aa0dd252697eb
yukio hirayama <suugakun@gmail.com>
2020-07-03 01:51:22 +0900

    Updates

---
+++ precache-manifest.c0418b6d9e2d63a185c7ba15e1a77f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca7ba44e9d41a9e1265c2ed2b6952a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a9a75188043b69bdc1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b12b7da109b8673a451d",
+    "url": "/static/js/2.b3070e4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3070e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a9a75188043b69bdc1",
+    "url": "/static/js/main.80ed7aa1.chunk.js"
+  },
+  {
+    "revision": "68202c30dc1fdd302a0b",
+    "url": "/static/js/runtime-main.b7c9a81b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
